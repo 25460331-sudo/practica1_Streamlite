e38ccba8d7cfe25ff99b7fbc5cfa16c25f6a94a0
Diego Reyes Natilla <25460331@colima.tecnm.mx>
2025-11-28 17:09:00 -0600

    Commit de practica 1

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Hola Mundo!")
+st.write("Esta es mi primera aplicación web creada con streamlit")
+
+st.subheader("Que hace esta aplicación?")
+st.write(
+    """
+    - Muestra un titulo en la parte superior
+    - Muestra un texto de bienvenida
+    - Corre como una app web en el navegador
+    """
+)
+
+st.success("Felicidades. Ya tienes tu primera app web con streamlit")
